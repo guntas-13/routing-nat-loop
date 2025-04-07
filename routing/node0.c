bdedffa3111b3d2c68054f0d4322bd88c0136ccb
guntas-13 <guntassingh.saran@iitgn.ac.in>
2025-04-08 02:24:00 +0530

    mostly I think node0.c done

--- routing/node0.c
+++ routing/node0.c
@@ -17,73 +17,114 @@ struct distance_table
 } dt0;
 
 
-/* students to write the following two routines, and maybe some others */
+int src_node = 0;
+int wts0[4] = {0, 1, 3, 7}; // direct costs to neighbors
 
-void rtinit0() 
+#define INF 999
+
+int min_macro0(int a, int b)
 {
-    /*
-    It should initialize the distance table in node 0 to reflect 
-    the direct costs of 1, 3, and 7 to nodes 1, 2, and 3, respectively. 
-    All links are bi-directional and the costs in both directions are identical. 
-    After initializing the distance table, and any other data structures needed 
-    by your node 0 routines, it should then send its directly-connected neighbors 
-    (in this case, 1, 2 and 3) the cost of it minimum cost paths, 
-    i.e., its distance vector, to all other network nodes. 
-    This minimum cost information is sent to neighboring nodes in a routing packet 
-    by calling the routine tolayer2()
-    */
-    
-    int src_node = 0;
-    int direct_costs[4] = {0, 1, 3, 7};
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            dt0.costs[i][j] = 999; // Initialize to a large value
-            if (i == src_node)
-                dt0.costs[i][j] = direct_costs[j];
-            else if (i == j)
-                dt0.costs[i][j] = 0; 
-                
-        }
-    }
-    
-    printdt0(&dt0);
+    return (a < b) ? a : b;
+}
 
-    struct rtpkt packet;
-    packet.sourceid = 0;
+int min_cost0(int* a)
+{
+    return min_macro0(min_macro0(a[0], a[1]), min_macro0(a[2], a[3]));
+}
+
+// Send the distance vector to all directly connected neighbors
+// min_costs[i] = min_cost(dt0.costs[i]);
+// i.e. For destinations marked on columns, minimum cost to reach them
+// from node 0 is the minimum of the costs to reach them via all other nodes
+int min_costs0[4];
+void make_distance_vector()
+{
+    for (int i = 0; i < 4; i++)
+        min_costs0[i] = min_cost0(dt0.costs[i]);
+}
+
+void send_packet0()
+{
     for (int i = 0; i < 4; i++)
     {
-        if (i == src_node || direct_costs[i] == 999) continue;
+        if (i == src_node) continue;
+        struct rtpkt packet;
+        packet.sourceid = src_node;
         packet.destid = i;
-        packet.mincost[0] = dt0.costs[0][0];
-        packet.mincost[1] = dt0.costs[0][1];
-        packet.mincost[2] = dt0.costs[0][2];
-        packet.mincost[3] = dt0.costs[0][3];
+        for (int j = 0; j < 4; j++)
+            packet.mincost[j] = min_costs0[j];
         tolayer2(packet);
     }
 }
 
+/* students to write the following two routines, and maybe some others */
+
+void rtinit0() 
+{
+    // distance_table initialization
+    // dt0.costs[i][j] = currently computed cost to node i via direct neighbor j for Node 0
+    // dt0.costs[i][i] = wts0[i] for direct neighbors
+    // dt0.costs[i][j] = INF for non-direct neighbors
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i == j) dt0.costs[i][j] = wts0[i];    
+            else dt0.costs[i][j] = INF;
+        }
+    }
+
+    printdt0(&dt0);
+
+    make_distance_vector();
+    send_packet0();
+}
+
+void send_packet_onchange0()
+{
+    // Check if the distance vector has changed
+    int changed = 0;
+    int old_min_costs[4];
+    for (int i = 0; i < 4; i++)
+        old_min_costs[i] = min_costs0[i];
+    
+    make_distance_vector();
+    for (int i = 0; i < 4; i++)
+    {
+        if (min_costs0[i] != old_min_costs[i])
+        {
+            changed = 1;
+            break;
+        }
+    }
+    
+    if (changed)
+        send_packet0();
+}
 
 void rtupdate0(rcvdpkt)
   struct rtpkt *rcvdpkt;
 {
-    /*
-    The values it receives in a routing packet from some other 
-    node j contain j's current shortest path costs (j's distance vector) 
-    to all other network nodes. rtupdate0() uses these received values to 
-    update its own "distance table." The distance table inside each node 
-    is the principal data structure used by the distance vector algorithm.
-    */
+    int src_node = rcvdpkt->sourceid;
+    int min_costs_from_dst[4];
+    for (int i = 0; i < 4; i++)
+        min_costs_from_dst[i] = rcvdpkt->mincost[i];
+    
+    // Update the distance table for node 0
+    // i.e. if node 0's own minimum cost to another node changes as a result of the update
+    // node 0 informs its directly connected neighbors of this change in minimum cost
+    // hence node 0's own minimum cost to another node src_node = dt0.costs[src_node][src_node] + min_costs_from_dst[i]
 
-    /*
-    As specified by the distance vector algorithm we studied in class and in the text, 
-    if node 0's own minimum cost to another node changes as a result of the update, 
-    node 0 informs its directly connected neighbors of this change in minimum cost 
-    by sending them a routing packet. Recall that in the distance vector algorithm, 
-    only directly connected nodes will exchange routing packets. Thus nodes 1 and 2 
-    will communicate with each other, but nodes 1 and 3 will node communicate with each other.
-    */
+    for (int i = 0; i < 4; i++)
+    {
+        int new_cost = dt0.costs[src_node][src_node] + min_costs_from_dst[i];
+        if (new_cost < INF)
+            dt0.costs[i][src_node] = new_cost;
+        else
+            dt0.costs[i][src_node] = INF;
+    }
+    printdt0(&dt0);
+    send_packet_onchange0();
 }
 
 
