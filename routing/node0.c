4ace65921f95c7b4abfd48ed21a36de69eddbd1c
guntas-13 <guntassingh.saran@iitgn.ac.in>
2025-04-08 21:29:45 +0530

    node0 done i feel

--- routing/node0.c
+++ routing/node0.c
@@ -45,14 +45,15 @@ void make_distance_vector()
 
 void send_packet0()
 {
+    struct rtpkt packet;
+    packet.sourceid = src_node;
+    for (int j = 0; j < 4; j++)
+        packet.mincost[j] = min_costs0[j];
+
     for (int i = 0; i < 4; i++)
     {
         if (i == src_node) continue;
-        struct rtpkt packet;
-        packet.sourceid = src_node;
         packet.destid = i;
-        for (int j = 0; j < 4; j++)
-            packet.mincost[j] = min_costs0[j];
         tolayer2(packet);
     }
 }
@@ -80,14 +81,31 @@ void rtinit0()
     send_packet0();
 }
 
-void send_packet_onchange0()
+void rtupdate0(rcvdpkt)
+  struct rtpkt *rcvdpkt;
 {
-    // Check if the distance vector has changed
+    int src_node = rcvdpkt->sourceid;
+
     int changed = 0;
     int old_min_costs[4];
     for (int i = 0; i < 4; i++)
         old_min_costs[i] = min_costs0[i];
     
+    // Update the distance table for node 0
+    // i.e. if node 0's own minimum cost to another node changes as a result of the update
+    // node 0 informs its directly connected neighbors of this change in minimum cost
+    // hence node 0's own minimum cost to another node src_node = dt0.costs[src_node][src_node] + min_costs_from_dst[i]
+
+    for (int i = 0; i < 4; i++)
+    {
+        int new_cost = dt0.costs[src_node][src_node] + rcvdpkt->mincost[i];
+        if (new_cost < INF)
+            dt0.costs[i][src_node] = new_cost;
+        else
+            dt0.costs[i][src_node] = INF;
+    }
+    printdt0(&dt0);
+
     make_distance_vector();
     for (int i = 0; i < 4; i++)
     {
@@ -100,31 +118,6 @@ void send_packet_onchange0()
     
     if (changed)
         send_packet0();
-}
-
-void rtupdate0(rcvdpkt)
-  struct rtpkt *rcvdpkt;
-{
-    int src_node = rcvdpkt->sourceid;
-    int min_costs_from_dst[4];
-    for (int i = 0; i < 4; i++)
-        min_costs_from_dst[i] = rcvdpkt->mincost[i];
-    
-    // Update the distance table for node 0
-    // i.e. if node 0's own minimum cost to another node changes as a result of the update
-    // node 0 informs its directly connected neighbors of this change in minimum cost
-    // hence node 0's own minimum cost to another node src_node = dt0.costs[src_node][src_node] + min_costs_from_dst[i]
-
-    for (int i = 0; i < 4; i++)
-    {
-        int new_cost = dt0.costs[src_node][src_node] + min_costs_from_dst[i];
-        if (new_cost < INF)
-            dt0.costs[i][src_node] = new_cost;
-        else
-            dt0.costs[i][src_node] = INF;
-    }
-    printdt0(&dt0);
-    send_packet_onchange0();
 }
 
 
